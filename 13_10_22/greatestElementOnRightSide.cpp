434f7af3809b20435889e25eac8216cdc1aea721
abhishekprasad2384 <49679013+abhishekprasad2384@users.noreply.github.com>
2022-10-13 18:01:39 +0530

    Create greatestElementOnRightSide.cpp

---
+++ 13_10_22/greatestElementOnRightSide.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int arr[] = {1,2,5,9,9,1,5,6,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+   
+    int maxi = INT_MIN;
+    for(int i =n-1;i>=0;i--){
+        maxi=max(arr[i],maxi);
+        arr[i]=maxi;
+    }
+    
+    for(int it : arr){
+        cout<<it<<endl;
+    }
+    return 0;
+}
