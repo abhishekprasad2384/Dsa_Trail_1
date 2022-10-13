ecc2e80583bc6b49609c4c9fae5b967266dcd83e
abhishekprasad2384 <49679013+abhishekprasad2384@users.noreply.github.com>
2022-10-13 17:43:41 +0530

    Create reverse.cpp

---
+++ 13_10_22/reverse.cpp
@@ -0,0 +1,10 @@
+void reverse(int arr[]){
+  int n = sizeof(arr)/sizeof(arr[0]);
+  int start =0;
+  int end =n-1;
+  while(start<=end){
+    swap(arr[start],arr[end]);
+    start++;
+    end--;
+  }
+}
